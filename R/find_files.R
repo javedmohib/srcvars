6e61c09fb5202b825e67dbd5fb12af34d322b0b8
jmohib <jmohib@srcentre.com.au>
2023-02-14 15:22:11 +1100

    Minor improvements

--- R/find_files.R
+++ R/find_files.R
@@ -8,13 +8,13 @@
 #' @return
 #' @export
 #'@examples
-#' find_files(folder_path = "K:/QILT/GOS/2023", subfol der_path = c("sample", "feb"), file_extension = "xlsx", file_name = c("Spec", "custom"), match_all = TRUE, strict = TRUE)
-find_files <- function(folder_path, subfolder_path = "", file_name, file_extension = "", match_all = TRUE, strict = FALSE){
+#' find_files(folder_path = "K:/QILT/GOS/2023", subfolder_path = c("sample", "feb"), file_extension = "xlsx", file_name = c("Spec", "custom"), match_all = TRUE, strict = TRUE)
+find_files <- function(folder_path, subfolder_path, file_name, file_extension, match_all = TRUE, strict = FALSE){
   all_files <- dir(folder_path, recursive = TRUE)
   matching_files <- character(0)
 
   for (file in all_files){
-    if (!grepl("^\\.", file) && # remove the files that start with "."
+    if (!grepl("^[\\.\\$~]", file) && # remove the files that start with "., ~, $"  (i.e. hidden files)
         grepl(paste0(".", file_extension), file, ignore.case = TRUE) && # select only those with desired extension
         ((match_all && all(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))) || # Runs when match_all = TRUE
          (!match_all && any(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))))){ # Runs when match_all = FALSE
@@ -33,7 +33,7 @@ find_files <- function(folder_path, subfolder_path = "", file_name, file_extensi
   }
 
   if (length(matching_files) == 0){
-    return(NULL)
+    return(paste0("There is no such file in the folder: ", folder_path))
   }
 
   cat("The following files were found:\n")
@@ -41,9 +41,9 @@ find_files <- function(folder_path, subfolder_path = "", file_name, file_extensi
     cat(paste0(i, ": ", matching_files[i], "\n"))
   }
 
-  file_index <- as.numeric(readline("Enter the number of the file to open: "))
+  file_index <- as.numeric(readline("Which file do you want to open? \n"))
   while (file_index < 1 || file_index > length(matching_files)) {
-    cat("Invalid input. Enter a number between 1 and ", length(matching_files), ": ")
+    cat("Invalid input. Enter a number between 1 and", length(matching_files), ": ")
     file_index <- as.numeric(readline(""))
   }
 
