f2777b6c22836c33df896be6b2e9f510ad742893
jmohib <jmohib@srcentre.com.au>
2023-02-22 13:32:25 +1100

    Some major tweaks in the function. Removed project path as function argument, using project and year instead

--- R/find_files.R
+++ R/find_files.R
@@ -1,39 +1,55 @@
-#' Title Find a file in a folder
-#' @param folder_path Path to the folder in which the file exists
-#' @param subfolder_path Sub-folder name in any order
+#' To find a file in QILT project folders
+#' @param project Specify the QILT project (GOS, SES, ESS, GOS-L)
+#' @param year Specify the collection year
+#' @param subfolder_path Sub-folder name of the file path in any order
 #' @param file_extension File extension for example xlsx or txt
-#' @param file_name A string from the name of the file
-#' @param match_all TRUE if all the values in file_name must be there in the output file.
-#' @param strict If TRUE only gives results when all values in subfolder_path match
+#' @param file_name One or more keywords from the name of the file
+#' @param match_all_name TRUE if all the values in file_name must be there in the output file.
+#' @param match_all_path If TRUE only gives results when all values in subfolder_path match
 #' @return
 #' @export
 #'@examples
-#' find_files(folder_path = "K:/QILT/GOS/2023", subfolder_path = c("sample", "feb"), file_extension = "xlsx", file_name = c("Spec", "custom"), match_all = TRUE, strict = TRUE)
-find_files <- function(folder_path, subfolder_path, file_name, file_extension, match_all = TRUE, strict = FALSE){
-  all_files <- dir(folder_path, recursive = TRUE)
+#' \dontrun{find_files(project = "GOS", year = "2023", subfolder_path = c("sample", "feb"), file_name = c("Spec", "sample"), file_extension = "xlsx", match_all_name = TRUE, match_all_path = TRUE)}
+find_files <- function(project, year, drive = "", subfolder_path = "", file_name = "", file_extension = "", match_all_name = TRUE, match_all_path = FALSE){
+  if(tolower(project) == "gosl") project <- "GOS-L"
+  if(tolower(drive) == "k"){
+    folder_paths <- glue::glue("K:/QILT/{project}/{year}")
+  } else if(tolower(drive) == "z"){
+    folder_paths <- glue::glue("//srcentre.local/drives/z/Consulting/Jobs/QILT/{project}/{year}")
+  } else {
+  folder_paths <- c(
+    glue::glue("K:/QILT/{project}/{year}"),
+    glue::glue("//srcentre.local/drives/z/Consulting/Jobs/QILT/{project}/{year}")
+  )
+  }
+
   matching_files <- character(0)
 
-  for (file in all_files){
-    if (!grepl("^[\\.\\$~]", file) && # remove the files that start with "., ~, $"  (i.e. hidden files)
-        grepl(paste0(".", file_extension), file, ignore.case = TRUE) && # select only those with desired extension
-        ((match_all && all(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))) || # Runs when match_all = TRUE
-         (!match_all && any(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))))){ # Runs when match_all = FALSE
-      if (subfolder_path == ""){
-        matching_files <- c(matching_files, file.path(folder_path, file))
-      } else {
-        subfolder_path <- sub("^.*/", "", file.path(folder_path, file))
-        subfolder_path <- sub("/[^/]+$", "", subfolder_path)
-        subfolder_path_components <- strsplit(subfolder_path, "/")[[1]]
-        if ((!strict && any(sapply(subfolder_path, function(arg) grepl(arg, subfolder_path_components, ignore.case = TRUE)))) ||
-            (strict && all(sapply(subfolder_path, function(arg) grepl(arg, subfolder_path_components, ignore.case = TRUE))))){
+  for (folder_path in folder_paths) {
+    all_files <- dir(folder_path, recursive = TRUE)
+
+    for (file in all_files){
+      if (!grepl("^[\\.\\$~]", file) && # remove the files that start with "., ~, $"  (i.e. hidden files)
+          grepl(paste0(".", file_extension), file, ignore.case = TRUE) && # select only those with desired extension
+          ((match_all_name && all(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))) || # Runs when match_all_name = TRUE
+           (!match_all_name && any(sapply(file_name, function(arg) grepl(arg, basename(file), ignore.case = TRUE)))))){ # Runs when match_all_name = FALSE
+        if (all(subfolder_path == "")){
           matching_files <- c(matching_files, file.path(folder_path, file))
+        } else {
+          # matching_subfolder_path <- sub("^.*/", "", file.path(folder_path, file))
+          matching_subfolder_path <- sub("/[^/]+$", "", file.path(folder_path, file))
+          # subfolder_path_components <- strsplit(matching_subfolder_path, "[/_  ]")[[1]]
+          if ((!match_all_path && any(sapply(subfolder_path, function(arg) grepl(arg, matching_subfolder_path, ignore.case = TRUE)))) ||
+              (match_all_path && all(sapply(subfolder_path, function(arg) grepl(arg, matching_subfolder_path, ignore.case = TRUE))))){
+            matching_files <- c(matching_files, file.path(folder_path, file))
+          }
         }
       }
     }
   }
 
   if (length(matching_files) == 0){
-    return(paste0("There is no such file in the folder: ", folder_path))
+    return(paste0("There is no such file in the folder: ", folder_paths))
   }
 
   cat("The following files were found:\n")
@@ -48,56 +64,4 @@ find_files <- function(folder_path, subfolder_path, file_name, file_extension, m
   }
 
   shell.exec(matching_files[file_index])
-}
-
-
-
-
-# tidyverse way
-# library(purrr)
-#
-# find_files_tidyverse <- function(folder_path, subfolder_path = "", file_name, file_extension, match_all = FALSE){
-#
-#   all_files <- dir(folder_path, recursive = TRUE)
-#
-#   matching_files <- all_files %>%
-#     keep(~ !grepl("^\\.", .x) &
-#            grepl(paste0(".", file_extension), .x, ignore.case = TRUE) &
-#            ((match_all & all(sapply(file_name, function(arg) grepl(arg, basename(.x), ignore.case = TRUE)))) |
-#               (!match_all & any(sapply(file_name, function(arg) grepl(arg, basename(.x), ignore.case = TRUE)))))) %>%
-#     map(~ if (subfolder_path == ""){
-#       file.path(folder_path, .x)
-#     } else {
-#       subfolder_path <- sub("^.*/", "", file.path(folder_path, .x))
-#       subfolder_path <- sub("/[^/]+$", "", subfolder_path)
-#       subfolder_path_components <- strsplit(subfolder_path, "/")[[1]]
-#       if (all(sapply(subfolder_path, function(arg) grepl(arg, subfolder_path_components, ignore.case = TRUE)))){
-#         file.path(folder_path, .x)
-#       } else {
-#         NULL
-#       }
-#     })
-#
-#   matching_files <- matching_files[!is.na(matching_files)]
-#
-#   if (length(matching_files) == 0){
-#     return(NULL)
-#   }
-#
-#   cat("The following files were found:\n")
-#   for (i in 1:length(matching_files)){
-#     cat(paste0(i, ": ", matching_files[i], "\n"))
-#   }
-#
-#   file_index <- as.numeric(readline("Enter the number of the file to open: "))
-#   while (file_index < 1 || file_index > length(matching_files)){
-#     file_index <- as.numeric(readline("Invalid input. Enter a number between 1 and ", length(matching_files), ": "))
-#   }
-#
-#   shell.exec(matching_files[file_index])
-# }
-
-find_files_snip <- function(){
-  string <- "find_files(folder_path = 'K:/QILT/GOS/2023', subfolder_path = c('sample', 'feb'), file_extension = 'xlsx', file_name = c('Spec', 'custom'), match_all = TRUE, strict = TRUE)"
-  cat(string)
 }
